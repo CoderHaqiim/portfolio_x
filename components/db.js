275a769557100922378013c1f84e03422fe7ad0f
Haqiim <hqcoder.jh@gmail.com>
2025-01-09 11:41:30 +0100

    #

--- components/db.js
+++ components/db.js
@@ -1,7 +1,7 @@
 const projects = [
     {
-        name:'music player app',
-        description:'A music player application built with Html, Css and javascript. Application has basic media player functionalities including playing modes and shuffle.',
+        name:'task-master',
+        description:`A multi-user task management application. Application can create, edit and delete tasks. It also recommends tasks based on priority and due date.<br><br> Use:<br>email-<em><strong>pseudo.mail@gmail.com</strong></em>;<br>password-<em><strong>Pseudo111</strong></em>.`,
         technologies:[
             {
                 name: 'html', img:'assets/svgs/html_x.svg'
@@ -11,12 +11,21 @@ const projects = [
             },
             {
                 name:"javascript", img:'assets/svgs/javascript_x.svg'
-            }
+            },
+            {
+                name:"node", img:'assets/svgs/node_x.svg'
+            },
+            {
+                name:"express", img:'assets/svgs/express_x.svg'
+            },
+            {
+                name:"mongoose", img:'assets/svgs/mongoose_x.svg'
+            },
         ],
-        imgUrl:'assets/images/player.png',
+        imgUrl:'assets/images/task-master.png',
         liveUrl:'https://haqiim-taskmaster.netlify.app/',
         githubUrl:'https://github.com/CoderHaqiim/player_x',
-        id: 5
+        id:0
     },
     {
         name:'music player app',
@@ -35,7 +44,7 @@ const projects = [
         imgUrl:'assets/images/player.png',
         liveUrl:'https://haqiim-player-x.netlify.app/',
         githubUrl:'https://github.com/CoderHaqiim/player_x',
-        id: 0
+        id: 1
     },
     {
         name:'space tourism website',
@@ -55,7 +64,7 @@ const projects = [
         imgUrl:'assets/images/space_tourism.png',
         liveUrl:'https://haqiim-space-tourism.netlify.app/',
         githubUrl:'https://github.com/CoderHaqiim/space-tourism',
-        id: 1
+        id:2
     },
     {
         name:'easyshop',
@@ -81,7 +90,7 @@ const projects = [
         imgUrl:'assets/images/e-commerce.png',
         liveUrl:'https://myeasyshop.netlify.app',
         githubUrl:'https://github.com/CoderHaqiim/e-commerce-website',
-        id: 2
+        id: 3
     },
     {
         name:'portfolio_x',
@@ -100,7 +109,7 @@ const projects = [
         imgUrl:'assets/images/portfolio.png',
         liveUrl:'https://haqiim-portfolio-x.netlify.app',
         githubUrl:'https://github.com/CoderHaqiim/portfolio_x',
-        id: 3
+        id: 4
     },
     {
         name:'wetube',
@@ -119,7 +128,7 @@ const projects = [
         imgUrl:'assets/images/wetube.png',
         liveUrl:'https://wetube-youtube-clone.netlify.app/',
         githubUrl:'https://github.com/CoderHaqiim/we_tube',
-        id: 4
+        id: 5
     }
 ]
 
